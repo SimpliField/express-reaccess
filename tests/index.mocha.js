4a708f8b4dea40ecbf6206933c43f44c5b5973f9
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-09-11 10:14:20 +0200

    Adding one more security test

--- tests/index.mocha.js
+++ tests/index.mocha.js
@@ -96,6 +96,24 @@ describe('reacesss should throw err', function() {
       });
     });
 
+    it('when there is a templated value with no value and several templates', function(done) {
+      testReq({
+        rightsObj: [{
+          path: '/foo/:bar.foo/:bar.foo',
+          methods: reaccess.ALL_MASK
+        }],
+        userProp: 'user.content',
+        userObj: {}
+      }, {
+        userProp: 'user.content'
+      }, '/foo//')
+      .expect(500, /Unauthorized access!/)
+      .end(function(err, res){
+        if(err) throw err;
+        done();
+      });
+    });
+
 });
 
 describe('reacesss should work', function() {
